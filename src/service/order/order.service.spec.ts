b114e836f243bcd4179aecd38c8b3bdfd2d76419
Sky <sky@sql.com.my>
2022-06-28 14:27:49 +0800

    Added order test case

--- src/service/order/order.service.spec.ts
+++ src/service/order/order.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { Order } from 'src/model/order.model';
 
 import { OrderService } from './order.service';
 
 describe('OrderService', () => {
   let service: OrderService;
+  let normalOrder = new Order(false)
+  let vipOrder = new Order(true)
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +15,33 @@ describe('OrderService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Adding order should be in pending list', () => {
+    service.addOrder(normalOrder)
+    expect(service._pendingOrders).toContain(normalOrder);
+  });
+
+  it('VIP should be sorted in front of normal', () => {
+    let input = [normalOrder, vipOrder]
+    let output = [vipOrder, normalOrder]
+    expect(service.sortOrder(input)).toEqual(output);
+  });
+
+  it('VIP should be in front of normal in pending list', () => {
+    service.addOrder(normalOrder)
+    service.addOrder(vipOrder)
+    expect(service._pendingOrders).toEqual([vipOrder, normalOrder]);
+  });
+
+  it('Processing order should not be in pending list', () => {
+    service.addOrder(normalOrder)
+    service.processOrder()
+    expect(service._pendingOrders.some(order => order === normalOrder)).toBeFalse();
+  });
+
+  it('Completed order should be in completed list', () => {
+    service.completeOrder(normalOrder)
+    expect(service._completedOrders).toContain(normalOrder);
   });
 });
